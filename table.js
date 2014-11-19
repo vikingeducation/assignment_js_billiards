9857a3d0c6314fe8bd2929a64d6a73ad88d0d5ae
Michael Alexander <alexandermw@gmail.com>
2014-11-19 14:27:36 -0800

    Update table.js

--- table.js
+++ table.js
@@ -1,1 +1,33 @@
-BB = BB || {}
+//Represent a Table object with another Javascript module. 
+//It should have properties like its total dimensions and an array of BilliardBalls it contains.
+var BB = BB || {};
+
+var BB.TableModule = (function(){
+
+  //defaults to these if init isn't run
+  var height = 600;
+  var width = 800;
+  var balls = [];
+  
+  function init(height, width, balls){
+    this.height = height;
+    this.width = width;
+    this.balls = balls;
+  };
+  
+  function ballCollection(){
+    return this.balls;
+  };
+  
+  function addBall(ball){
+    this.balls.push(ball);
+  };
+
+  return {
+    init : init,
+    ballCollection : ballCollection,
+    addBall : addBall,
+    height: height,
+    width : width
+  };
+}());
