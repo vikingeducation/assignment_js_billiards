e8d754abb8599e8f13c9f8520c94ea9db8f4dd08
Michael Alexander <alexandermw@gmail.com>
2014-11-20 15:01:42 -0800

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ BB.MainModule = (function(){
     //initialize each ball and push onto the table
     for(var i = 1; i <= numBalls; i++){
       var ball = new BB.BilliardBallModule.BilliardBall();
-      ball.position = { x: 200, y : 800 - (i * 200) }; //spread out billiardBalls vertically
-      ball.velocity = { x: 4, y: 0};
+      ball.position = { x: 200, y : 600 - (i * 150) }; //spread out billiardBalls vertically
+      ball.velocity = { x: (i * 2), y: 0}; //vary speeds
       BB.TableModule.addBall(ball);
     };
 
